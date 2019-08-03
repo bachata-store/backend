9e3d985088191a6b39f66980b5132d4cfae6fe61
DI (FH) Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2019-08-03 18:15:55 -0400

    initial meteor scaffold;

---
+++ imports/ui/Hello.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Hello extends Component {
+  state = {
+  }
+
+  render() {
+    return (
+			<img src="assets/img/logo.png" />
+    );
+  }
+}
